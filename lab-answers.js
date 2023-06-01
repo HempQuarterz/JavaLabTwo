45145ad9422f9959cc5d79b0e03348b10ac80a6a
HempQuarterz <hempquarterz@gmail.com>
2023-05-31 18:50:06 -0700

    Fizz Buzz answered

--- lab-answers.js
+++ lab-answers.js
@@ -16,3 +16,17 @@
 //         console.log(i); 
 //     }
 // }
+
+////////////////////////////////////////////
+// Fizz Buzz
+////////////////////////////////////////////
+
+for (let i = 0; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 ===0)
+    console.log("FizzBuzz") 
+    if (i % 3 === 0) 
+        console.log("Fizz") 
+    if (i % 5 === 0) 
+        console.log("Buzz") 
+    else (console.log(i))
+}
