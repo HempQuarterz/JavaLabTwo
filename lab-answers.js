09306b2a6d1e56416066eba7cfde43ea608e6ba9
HempQuarterz <hempquarterz@gmail.com>
2023-05-31 20:30:53 -0700

    Wild Wild Life answered

--- lab-answers.js
+++ lab-answers.js
@@ -21,12 +21,32 @@
 // Fizz Buzz
 ////////////////////////////////////////////
 
-for (let i = 0; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 ===0)
-    console.log("FizzBuzz") 
-    if (i % 3 === 0) 
-        console.log("Fizz") 
-    if (i % 5 === 0) 
-        console.log("Buzz") 
-    else (console.log(i))
-}
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 ===0)
+//     console.log("FizzBuzz") 
+//     if (i % 3 === 0) 
+//         console.log("Fizz") 
+//     if (i % 5 === 0) 
+//         console.log("Buzz") 
+//     else (console.log(i))
+// }
+
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2] = 5001;
+console.log(plantee);
+
+wolfy[3] = "Gotham City";
+wolfy[0] = "Gameboy";
+console.log(wolfy);
+
+dart.push("Hawkins");
+console.log(dart);
+
+
+
