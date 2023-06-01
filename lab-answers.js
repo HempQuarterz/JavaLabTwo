31ae5dde72e540bb061cc55b3e729d1a123ae7a8
HempQuarterz <hempquarterz@gmail.com>
2023-05-31 20:47:20 -0700

    Yell at the Ninja Turtles

--- lab-answers.js
+++ lab-answers.js
@@ -32,21 +32,26 @@
 // }
 
 
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
-plantee[2] = 5001;
-console.log(plantee);
+// plantee[2] = 5001;
+// console.log(plantee);
 
-wolfy[3] = "Gotham City";
-wolfy[0] = "Gameboy";
-console.log(wolfy);
+// wolfy[3] = "Gotham City";
+// wolfy[0] = "Gameboy";
+// console.log(wolfy);
 
-dart.push("Hawkins");
-console.log(dart);
+// dart.push("Hawkins");
+// console.log(dart);
 
+let ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+const allCaps = []
 
-
+for (let item of ninjaTurtles) {
+    allCaps.push(item.toUpperCase());
+}
+console.log(allCaps)
